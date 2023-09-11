8e2df24d69e3b48c00c20a2f150c8d4ad05dd48b
michael <mdjcosta30@gmail.com>
2023-09-11 15:23:17 +0200

    part 2 solution - changed walk to add every visited point to the map for this solution

--- adventOfCode2016/day01/main.go
+++ adventOfCode2016/day01/main.go
@@ -86,7 +86,7 @@ func splitInstruction(instruction string) []string {
 }
 
 // distanceBetweenTwoPoints gets the line drawn between the two points distance
-func distanceBetweenTwoPoints(p1, p2 point) float64 { 
+func distanceBetweenTwoPoints(p1, p2 point) float64 {
 	return math.Sqrt(math.Pow(float64(p2.x-p1.x), 2) + math.Pow(float64(p2.y-p1.y), 2))
 }
 
@@ -94,7 +94,7 @@ func distanceBetweenTwoPoints(p1, p2 point) float64 {
 // on a grid. Alg taken from: https://www.wikiwand.com/en/Taxicab_geometry
 // note: the p1 is redundant here because we always start at origin, but this works even if we start at a different point.
 func distanceBetweenTwoPointsTaxicabGrid(p1, p2 point) float64 {
-  return math.Abs(float64(p1.x) - float64(p2.x)) + math.Abs(float64(p1.y) - float64(p2.y))
+	return math.Abs(float64(p1.x)-float64(p2.x)) + math.Abs(float64(p1.y)-float64(p2.y))
 }
 
 func main() {
@@ -114,35 +114,61 @@ func main() {
 		y: 0,
 	}
 
+	// For the second part (the bad section of code below)
+	visitedPoints := []point{origin}
+	foundFirst := false
+	firstDouble := point{}
+
 	for x := 0; x < len(d); x++ {
 		instructions := splitInstruction(d[x])
 
 		// first change dir, then walk.
 		dir.change(instructions[0])
-    // Issue was not parsing the entire remaining split for the whole number (numbers with more than 2 digits...)
-    tempNum := strings.Join(instructions[1:], "")
+		// Issue was not parsing the entire remaining split for the whole number (numbers with more than 2 digits...)
+		tempNum := strings.Join(instructions[1:], "")
 		units, err := strconv.Atoi(tempNum)
 		if err != nil {
 			panic(err)
 		}
 
-		if dir.loc == "up" {
-			walkPoint.up(units)
-		}
-		if dir.loc == "down" {
-			walkPoint.down(units)
-		}
-		if dir.loc == "left" {
-			walkPoint.left(units)
-		}
-		if dir.loc == "right" {
-			walkPoint.right(units)
+		for x := 0; x < units; x++ {
+			if dir.loc == "up" {
+				walkPoint.up(1)
+			}
+			if dir.loc == "down" {
+				walkPoint.down(1)
+			}
+			if dir.loc == "left" {
+				walkPoint.left(1)
+			}
+			if dir.loc == "right" {
+				walkPoint.right(1)
+			}
+
+			// Add the point, and check if we've been there before.
+			// This is a very slow solution (well naive - at least)
+
+			if !foundFirst {
+				for x := 0; x < len(visitedPoints); x++ {
+					if visitedPoints[x].x == walkPoint.x && visitedPoints[x].y == walkPoint.y {
+						foundFirst = true
+						firstDouble = walkPoint
+						fmt.Println("first double point: ", firstDouble)
+					}
+				}
+
+				visitedPoints = append(visitedPoints, walkPoint)
+			}
 		}
 	}
 
-	fmt.Println(walkPoint)
-  // Works, but we can't go diagonally - we have to go across the grids.
-	fmt.Println(distanceBetweenTwoPoints(origin, walkPoint))
-  // This makes use of the logic of taxi cab grid movement, i.e. only moving in a grid.
-	fmt.Println(distanceBetweenTwoPointsTaxicabGrid(origin, walkPoint))
+	// Part 1
+	fmt.Println("final location: ", walkPoint)
+	// Works, but we can't go diagonally - we have to go across the grids.
+	fmt.Println("straight line distance from start to final point: ", distanceBetweenTwoPoints(origin, walkPoint))
+	// This makes use of the logic of taxi cab grid movement, i.e. only moving in a grid.
+	fmt.Println("taxi cab grid distance between start and final point: ", distanceBetweenTwoPointsTaxicabGrid(origin, walkPoint))
+
+	// Part 2
+	fmt.Println("taxi cab grid distance between start and first duplicate (visited twice) point: ", distanceBetweenTwoPointsTaxicabGrid(origin, firstDouble))
 }
